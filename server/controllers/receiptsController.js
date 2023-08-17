3562941d0db9b224d5fc5df3e9dd23be7839ddf8
Pasha <zenoken4ik@gmail.com>
2023-08-17 17:34:41 +0300

    added changeProductQuantity controller

--- server/controllers/receiptsController.js
+++ server/controllers/receiptsController.js
@@ -2,69 +2,119 @@ const Product = require("../models/Product");
 const ProductInReceipt = require("../models/ProductInReceipt");
 const Receipt = require("../models/Receipt");
 
-
 const createReceipt = async (req, res) => {
-    try {
-        const { productId } = req.body;
+  try {
+    const { productId } = req.body;
 
-        const product = await Product.findByPk(productId);
-        if (!product) {
-            return res.status(404).json({ error: 'Product not found' });
-        }
-
-        const total = product.price
-        const receipt = await Receipt.create({ total });
-        res.status(201).json(receipt);
-
-        await ProductInReceipt.create({
-            ReceiptId: receipt.id,
-            ProductId: product.id,
-            quantity: 1,
-            price: product.price,
-        });
-      
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal server error' });
+    const product = await Product.findByPk(productId);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
     }
-}
+
+    const total = product.price;
+    const receipt = await Receipt.create({ total });
+    res.status(201).json(receipt);
+
+    await ProductInReceipt.create({
+      ReceiptId: receipt.id,
+      ProductId: product.id,
+      quantity: 1,
+      price: product.price,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 const addProductToReceipt = async (req, res) => {
-    try {
-      const { receiptId } = req.params;
-      const { productId } = req.body;
-  
-      const receipt = await Receipt.findByPk(receiptId);
-      const product = await Product.findByPk(productId);
-  
-      if (!receipt) {
-        return res.status(404).json({ error: 'Receipt not found' });
-      }
-  
-      if (!product) {
-        return res.status(404).json({ error: 'Product not found' });
-      }
-  
-      const totalPrice = product.price
-  
-      const productInReceipt = await ProductInReceipt.create({
-        ReceiptId: receipt.id,
-        ProductId: product.id,
-        quantity: 1,
-        price: product.price, 
-      });
-  
-      receipt.total += totalPrice;
-      await receipt.save();
-  
-      res.status(201).json(productInReceipt);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal server error' });
+  try {
+    const { receiptId } = req.params;
+    const { productId } = req.body;
+
+    const receipt = await Receipt.findByPk(receiptId);
+    const product = await Product.findByPk(productId);
+
+    if (!receipt) {
+      return res.status(404).json({ error: "Receipt not found" });
     }
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    const totalPrice = product.price;
+
+    const productInReceipt = await ProductInReceipt.create({
+      ReceiptId: receipt.id,
+      ProductId: product.id,
+      quantity: 1,
+      price: product.price,
+    });
+
+    receipt.total += totalPrice;
+    await receipt.save();
+
+    res.status(201).json(productInReceipt);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
+};
+
+const changeProductQuantity = async (req, res) => {
+  try {
+    const { receiptId, productId } = req.params;
+    const { action, quantity } = req.body;
+
+    const receipt = await Receipt.findByPk(receiptId);
+    const productInReceipt = await ProductInReceipt.findOne({
+      where: { ReceiptId: receiptId, ProductId: productId },
+    });
+
+    if (!productInReceipt) {
+      return res.status(404).json({ error: "Product not found in receipt" });
+    }
+
+    if (action === "add") {
+      productInReceipt.quantity += quantity;
+    } else if (action === "remove") {
+      productInReceipt.quantity -= quantity;
+    } else if (action === "update") {
+      productInReceipt.quantity = quantity;
+    } else if (action === "delete") {
+      receipt.total -= (productInReceipt.price * productInReceipt.quantity);
+      await receipt.save();
+      await productInReceipt.destroy();
+      return res.json({ message: "Product removed from receipt" });
+
+    } else {
+      return res.status(400).json({ error: "Bad request" });
+    }
+
+    await productInReceipt.save();
+
+    const productsInReceipt = await ProductInReceipt.findAll({
+      where: { ReceiptId: receipt.id },
+    });
+    let totalPrice = 0;
+    productsInReceipt.forEach((product) => {
+      totalPrice += product.price * product.quantity;
+    });
+
+    receipt.total = totalPrice;
+    await receipt.save();
+
+    res.json(productInReceipt);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 module.exports = {
-    createReceipt,
-    addProductToReceipt
-}
+  createReceipt,
+  addProductToReceipt,
+  changeProductQuantity,
+};
--- server/routes/receiptsRoute.js
+++ server/routes/receiptsRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createReceipt, addProductToReceipt } = require('../controllers/receiptsController')
+const { createReceipt, addProductToReceipt, changeProductQuantity } = require('../controllers/receiptsController')
 
 
 router.post('/', createReceipt)
-router.put('/:receiptId', addProductToReceipt)
+router.patch('/:receiptId', addProductToReceipt)
+router.patch('/:receiptId/products/:productId', changeProductQuantity)
 
 
 module.exports = router;
